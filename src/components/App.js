864f5ecc25c73ce99b95300986afa388b0f50b12
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-03-07 18:18:12 +0000

    sol1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,102 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
+indian_cities = [
+  "Mumbai",
+  "Delhi",
+  "Bangalore",
+  "Hyderabad",
+  "Chennai",
+  "Kolkata",
+  "Ahmedabad",
+  "Pune",
+  "Surat",
+  "Jaipur",
+  "Lucknow",
+  "Kanpur",
+  "Nagpur",
+  "Indore",
+  "Thane",
+  "Bhopal",
+  "Visakhapatnam",
+  "Pimpri-Chinchwad",
+  "Patna",
+  "Vadodara",
+  "Ghaziabad",
+  "Ludhiana",
+  "Agra",
+  "Nashik",
+  "Faridabad",
+  "Meerut",
+  "Rajkot",
+  "Kalyan-Dombivali",
+  "Vasai-Virar",
+  "Varanasi",
+  "Srinagar",
+  "Aurangabad",
+  "Dhanbad",
+  "Amritsar",
+  "Navi Mumbai",
+  "Allahabad",
+  "Ranchi",
+  "Howrah",
+  "Jabalpur",
+  "Gwalior",
+  "Vijayawada",
+  "Jodhpur",
+  "Madurai",
+  "Raipur",
+  "Kota",
+  "Chandigarh",
+  "Guwahati",
+  "Solapur",
+  "Hubli-Dharwad",
+  "Tiruchirappalli",
+  "Bareilly"
+ 
+]
+
+
 const App = () => {
+
+const [cities,setCities]=useSate(indian_cities);
+const [search,setSearch]=useState("");
+const [click,setClick]=useState(false);
+useEffect(()=>{
+
+search && setCities(indian_cities.filter(city =>city.toLowercase().includes(search.trim().toLocaleLowerCase())))
+},[search]);
+
+
+function updateSearch(city){
+setSearch(city);
+setClick(true);
+}
+
+
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Search citiesof India :</h1>
+        <form>
+          <input type="text" 
+          onChange={e=>{
+            setSearch(e.target.value) 
+            setClick(false) }} 
+            value={search}  placeholder="Search..."/>
+        </form>
+
+        { !click &&
+           <ul>
+           {cities && cities.map((city,index)=>(
+           <li key={index} onClick={()=>updateSearch(city)}>{city}</li>
+           )
+           )}
+         </ul>
+        }
+       
     </div>
   )
 }
